49212ea5e5d5fd59c324c42e9af63bacae298f51
Harishankarreddy01 <harishankarreddy02@gmail.com>
2024-09-25 15:44:32 -0400

    Declaring Classes

---
+++ ClassesAndObjects.java
@@ -0,0 +1,19 @@
+//CLASSES AND OBJECTS
+// CLASSES
+/* DECLARING CLASSES:
+    Modifiers such as public, privates and a number of others
+    The class name, with the initial letter capitalized by convention.
+    The name of the class's parent(superclass), if any, preceded by the keyword extends. A class can only extend one parent.
+    A comma-separated list of interfaces implemented by the class, if any, preceded by the keyword implements. A class can implement more than one interface.
+    A class body surrounded by braces, {}.
+
+    Example:
+    public class MyClass extends MySuperClass implements YourInterface {
+        field, constructor, and
+        method declarations.
+    }
+
+    constructor: initializing new objects
+    fields: provide the state of the class and its objects
+    methods: To implement the behavior of the class and its objects.
+ */
