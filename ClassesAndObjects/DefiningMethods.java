01504349501185fcb80e67d5bc51401592822aa4
Harishankarreddy01 <harishankarreddy02@gmail.com>
2024-10-01 16:48:15 -0400

    Method declaration

---
+++ ClassesAndObjects/DefiningMethods.java
@@ -0,0 +1,34 @@
+package ClassesAndObjects;
+
+/* DEFINING METHODS
+    1. Modifiers
+    2. Return type
+    3. Method Name
+    4. list of parameters in parenthesis
+    5. An exception list
+    6. Method body
+
+    Method signature: Method name and the parameters type.
+                    Example: calculateAnswer(double, int, double, double)
+
+    NAMING A METHOD:
+    -> Method names should be a verb in lowercase.
+    -> Multi-word name should begin with a verb in lowercase.
+    -> In Multi-word name: The first letter of each of the second and following words should be capitalized.
+    examples:
+    run
+    runFast
+    getBackground
+    getFinalData
+    compareTo
+    setX
+    isEmpty
+
+
+    METHOD OVERLOADING:
+
+    Java can distinguish between methods with different method signatures.
+    Overloaded methods are differentiated by the number and the type of the arguments passed into the method.
+    You cannot declare more than one method with the same name and the same (number and type) of arguments, because the compiler cannot tell them apart.
+    The compiler does not consider return type when differentiating methods, so you cannot declare two methods with the same signature even if they have a different return type.
+ */
